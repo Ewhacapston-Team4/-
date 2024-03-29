4c1e3b9078042931d301f8fd5abfeda3d67a7d84
Shin Danah <shindanah@ewhain.net>
2024-03-29 14:49:21 +0900

    fourth commit

--- components/camera/ImagePicker.js
+++ components/camera/ImagePicker.js
@@ -1,3 +1,20 @@
-function ImagePicker() {}
+import { Button, View, StyleSheet } from "react-native";
+import { launchCameraAsync, useCameraPermissions } from "expo-image-picker";
+
+function ImagePicker() {
+  useCameraPermissions();
+
+  async function takeImageHandler() {
+    const image = await launchCameraAsync({ aspect: [1, 1], quality: 0.5 });
+    console.log(image);
+  }
+
+  return (
+    <View>
+      <Button title="사진 찍기" onPress={takeImageHandler} />
+      <Button title="갤러리에서 고르기" />
+    </View>
+  );
+}
 
 export default ImagePicker;
--- screens/SearchName.js
+++ screens/SearchName.js
@@ -6,7 +6,6 @@ function SearchName() {
   return (
     <View>
       <Title>이름으로 검색</Title>
-      <Button title="촬영"></Button>
     </View>
   );
 }
